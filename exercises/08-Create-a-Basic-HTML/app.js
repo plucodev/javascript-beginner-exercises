e9dc95b1d75369406016c480b1b58a297dd99dc1
Paolo Lucano <paololucanodeveloper@gmail.com>
2019-09-17 14:42:22 +0000

    completed 08

---
+++ exercises/08-Create-a-Basic-HTML/app.js
@@ -0,0 +1,11 @@
+const a = '</title>';
+const b = '</html>';
+const c = '<head>';
+const d = '</body>';
+const e = '<html>';
+const f = '</head>';
+const g = '<title>';
+const h = '<body>';
+
+// DON'T CHANGE THE CODE ABOVE
+
